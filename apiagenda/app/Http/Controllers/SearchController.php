47537070cd41e40b03b4a2dc9a0de0dd98f1be9e
ilocbjose <joseba_urizarbarrena_apps1ta1920@cev.com>
2021-02-20 22:31:36 +0100

    Cambio en search

--- apiagenda/app/Http/Controllers/SearchController.php
+++ apiagenda/app/Http/Controllers/SearchController.php
@@ -27,17 +27,17 @@ class SearchController extends Controller
 			if($data->contact_name){
 				$contact_name = $data->contact_name;
 				$contact = Contact::where('contact_name',$contact_name);
-					return response($contact,200);
+					return response()->json($contact,200);
 
 			}elseif($data->contact_phone){
 				$contact_phone = $data->contact_phone;
 				$contact = Contact::where('contact_name',$contact_name);
-					return response($contact,200);
+					return response()->json($contact,200);
 
 			}elseif($data->contact_email){
 				$contact_email = $data->contact_email;
 				$contact = Contact::where('contact_name',$contact_phone);
-					return response($contact,200);
+					return response()->json($contact,200);
 			}else{
 
 				return response('No existe ningún usuario.');
@@ -47,39 +47,6 @@ class SearchController extends Controller
 		} else {
 
 			return response('Datos introducidos nulos.');
-
-		}
-
-	}
-
-	public function index(Request $request){
-
-		$response = "";
-		$data = $request->getContent();
-
-		$data = json_decode($data);
-
-		if($data){
-
-			$user = JWTAuth::parseToken()->authenticate();
-
-			if($user){
-
-				$id_user = $user->id;
-
-				$contacts = Contacts::where('id',$id_user)->get();
-
-				return response($contacts);
-
-			}else{
-
-				return response('Datos del usuario erroneos.');
-
-			}
-
-		}else{
-
-			return response('No existen usuarios asociados ese ID.');
 
 		}
 
