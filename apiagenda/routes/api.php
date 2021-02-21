d5884cc0d8a76442445345ac0ecf00424bd13dfd
ilocbjose <joseba_urizarbarrena_apps1ta1920@cev.com>
2021-02-21 13:10:05 +0100

    Procfile updated

--- apiagenda/routes/api.php
+++ apiagenda/routes/api.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('register', 'App\Http\Controllers\UserController@register');
 Route::post('login', 'App\Http\Controllers\UserController@authenticate');
-////
+
 Route::post('password/email', 'App\Http\Controllers\UserController@forgot');
 Route::post('password/reset', 'App\Http\Controllers\UserController@reset');
 
@@ -30,14 +30,12 @@ Route::group(['middleware' => ['jwt.verify']], function() {
     
 	Route::get('user','App\Http\Controllers\UserController@getAuthenticatedUser');
     Route::get('logout','App\Http\Controllers\UserController@logout');
-    ////
     Route::post('create','App\Http\Controllers\CrudController@createContact');
     Route::post('eraseContact','App\Http\Controllers\CrudController@eraseContact');
     Route::post('eraseUser', 'App\Http\Controllers\UserController@eraseUser');
     Route::get('showContact', 'App\Http\Controllers\CrudController@showContact');
     Route::get('updateContact/{id}','App\Http\Controllers\CrudController@updateContact');
     Route::get('updateUser/{id}','App\Http\Controllers\CrudController@updateUser');
-    ////
     Route::get('search','App\Http\Controllers\SearchController@search');
 
 });
